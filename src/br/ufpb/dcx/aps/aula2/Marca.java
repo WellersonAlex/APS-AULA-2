fea3eaa5246d59a2e3ded9b0d723d9c8118607c2
WellersonAlex <wellerson.alex@dce.ufpb.br>
2019-06-11 11:39:42 -0300

    Cadastro de marcas

---
+++ src/br/ufpb/dcx/aps/aula2/Marca.java
@@ -0,0 +1,25 @@
+package br.ufpb.dcx.aps.aula2;
+
+public class Marca {
+	
+	private String nome;
+
+	public Marca(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return "Marca [nome=" + nome + "]";
+	}
+
+}
